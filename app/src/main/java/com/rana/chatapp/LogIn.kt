295357d0c92e78eaf381c6922c6eabaf09bffd6f
Rana Hisham <hishamrana00@gmail.com>
2021-10-29 23:35:14 +0200

    second commit(firebase connected) to be continued

--- app/src/main/java/com/rana/chatapp/LogIn.kt
+++ app/src/main/java/com/rana/chatapp/LogIn.kt
@@ -1,41 +1,47 @@
 package com.rana.chatapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.rana.chatapp.databinding.ActivityLoginBinding
 
 class LogIn : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var email: EditText
-    private lateinit var password : EditText
-    private lateinit var logIn: Button
-    private lateinit var signUp : Button
+    private lateinit var password: EditText
+    private lateinit var logInBtn: Button
+    private lateinit var signUpBtn: Button
 
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-//        email = binding.emailEt
-//        password = binding.passwordEt
-//        logIn = binding.loginBtn
-//        signUp = binding.signUpBtn
+        email = findViewById(R.id.emailEt)
+        password = findViewById(R.id.passwordEt)
+        logInBtn = findViewById(R.id.loginBtn)
+        signUpBtn = findViewById(R.id.signUpBtn)
+        mAuth = FirebaseAuth.getInstance() //this is how we initialize firebase object
 
-        email=findViewById(R.id.emailEt)
-        signUp = findViewById(R.id.signUpBtn)
-        logIn = findViewById(R.id.loginBtn)
-        signUp = findViewById(R.id.signUpBtn)
+        signUpBtn.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
 
+            startActivity(intent)
+        }
 
-       signUp.setOnClickListener{
-             val intent = Intent(this, signUp :: class.java)
+        logInBtn.setOnClickListener{
+            val email = email.text.toString()
+            val password = password.text.toString()
+            logIn(email,password)
+        }
 
-           startActivity(intent)
+    }
 
-       }
+    private fun logIn(email : String , password :String){
 
     }
 }
--- app/src/main/java/com/rana/chatapp/SignUp.kt
+++ app/src/main/java/com/rana/chatapp/SignUp.kt
@@ -2,13 +2,42 @@ package com.rana.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.rana.chatapp.databinding.ActivityLoginBinding
 import com.rana.chatapp.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
 
+
+    private lateinit var name :EditText
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var signUpBtn: Button
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        name = findViewById(R.id.nameEt)
+        email = findViewById(R.id.emailEt)
+        password = findViewById(R.id.passwordEt)
+        signUpBtn = findViewById(R.id.signUpBtn)
+        mAuth = FirebaseAuth.getInstance() //this is how we initialize firebase object
+
+        signUpBtn.setOnClickListener{
+            val email = email.text.toString()
+            val password = password.text.toString()
+//            val name = name.text.toString()
+
+
+            signUp(email,password)
+        }
+    }
+
+    private fun signUp (email : String , password :String){
+
     }
 }
