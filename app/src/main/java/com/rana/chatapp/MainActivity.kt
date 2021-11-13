4fa6b7b506d2ff46a89d697b3feb38cd0b6f7dc6
Rana Hisham <hishamrana00@gmail.com>
2021-11-13 23:32:40 +0200

    save

--- app/src/main/java/com/rana/chatapp/MainActivity.kt
+++ app/src/main/java/com/rana/chatapp/MainActivity.kt
@@ -1,40 +1,54 @@
 package com.rana.chatapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.rana.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var userRecyclerView: RecyclerView
-private lateinit var userList :ArrayList<UserData>
-private lateinit var adapter : UserLayoutAdapter
-private lateinit var mAuth: FirebaseAuth
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<UserData>
+    private lateinit var adapter: UserLayoutAdapter
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef : DatabaseReference
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
- mAuth = FirebaseAuth.getInstance()
-        userList  = ArrayList()
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        userList = ArrayList()
         adapter = UserLayoutAdapter(this, userList)
+
+        userRecyclerView = findViewById(R.id.recyclerView)
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
+        menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if(item.itemId == R.id.logOut){
+        if (item.itemId == R.id.logOut) {
             //the user will log out
-                mAuth.signOut()
+            mAuth.signOut()
+            val intent = Intent(this, LogIn::class.java)
             finish()
-                return true
+            startActivity(intent)
+            return true
+        }
+        return true
     }
-    return true
-} }
+}
